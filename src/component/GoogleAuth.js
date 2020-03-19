278062152e4d2511476a6c7323f12d3549ee0cbd
Soria <soriasayer@gmail.com>
2020-03-19 22:50:32 +0100

    Form: redux-form is added to the application

--- src/component/GoogleAuth.js
+++ src/component/GoogleAuth.js
@@ -19,7 +19,7 @@ class GoogleAuth extends Component {
 
     onAuthChange = (isSignedIn) => {
         if(isSignedIn) {
-            this.props.signIn()
+            this.props.signIn(this.auth.currentUser.get().getId())
         } else {
             this.props.signOut()
         }
@@ -62,7 +62,7 @@ class GoogleAuth extends Component {
 }
 
 const mapStateToProps = ({auth}) => {
-    
+
     return{
         isSignedIn: auth.isSignedIn,
     }
--- src/component/streams/StreamCreate.js
+++ src/component/streams/StreamCreate.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
+class StreamCreate extends Component {
+    renderInput({input, label}) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        )
+    }
 
-    return(
-        <div>StreamCreate</div>
-    )
+    onSubmit(formValue) {
+        console.log(formValue)
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field name='title'
+                 component={this.renderInput}
+                 label="Enter Title"
+                />
+                <Field name='description'
+                 component={this.renderInput}
+                 label="Enter Description"
+                />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate
+
+
+export default reduxForm({
+    form: 'createStream',
+    
+})(StreamCreate)
