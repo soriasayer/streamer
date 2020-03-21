1150cd43a3f4ac2bf4f5a8ee30e52a85d641baf0
Soria <soriasayer@gmail.com>
2020-03-21 14:22:52 +0100

    Post: Post request is created.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,22 @@
-import { SIGN_IN, SIGN_OUT } from './types'
-export const signIn = (userId) => {
-    return{
+import streams from './api/streams'
+import {
+    SIGN_IN,
+    SIGN_OUT
+} from './types'
+
+export const signIn = ( userId ) => {
+    return {
         type: SIGN_IN,
         payload: userId
     }
 }
 
 export const signOut = () => {
-    return{
+    return {
         type: SIGN_OUT
     }
+}
+
+export const createStream = formValues => async dispatch => {
+    streams.post( '/streams', formValues )
 }
--- src/component/streams/StreamCreate.js
+++ src/component/streams/StreamCreate.js
@@ -1,62 +1,73 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { createStream } from '../../actions'
 
 class StreamCreate extends Component {
-    renderError({error, touched}) {
-        if(touched && error) {
-            return(
-                <div className="ui error message">
-                    <div className="header">{error}</div>
-                </div>
-            )
-        }
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      )
     }
+  }
 
-    renderInput = ({input, label, meta}) => {
-        const className = `field ${meta.errors && meta.touched ? 'error' : '' }`
-        return (
-            <div className={className}>
-                <label>{label}</label>
-                <input {...input} autoComplete='off' />
-                {this.renderError(meta)}
-            </div>
-        )
-    }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${
+      meta.errors && meta.touched ? 'error' : ''
+    }`
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    )
+  }
 
-    onSubmit(formValue) {
-        console.log(formValue)
-    }
+  onSubmit = formValue => {
+    this.props.createStream(formValue)
+  }
 
-    render() {
-        return(
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                <Field name='title'
-                 component={this.renderInput}
-                 label="Enter Title"
-                />
-                <Field name='description'
-                 component={this.renderInput}
-                 label="Enter Description"
-                />
-                <button className="ui button primary">Submit</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field
+          name="title"
+          component={this.renderInput}
+          label="Enter Title"
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    )
+  }
 }
 
-const validate = (formValues) => {
-    const errors = {}
-    if(!formValues.title) {
-        errors.title = 'You must enter a title.'
-    }
+const validate = formValues => {
+  const errors = {}
+  if (!formValues.title) {
+    errors.title = 'You must enter a title.'
+  }
 
-    if(!formValues.description) {
-        errors.description = 'You must enter a description.'
-    }
-    return errors
+  if (!formValues.description) {
+    errors.description = 'You must enter a description.'
+  }
+  return errors
 }
 
-export default reduxForm({
-    form: 'createStream',
-    validate
+const formWrapped = reduxForm({
+  form: 'createStream',
+  validate,
 })(StreamCreate)
+
+export default connect(null, { createStream })(formWrapped)
