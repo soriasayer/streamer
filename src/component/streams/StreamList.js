585147a004a551dd9739db51b17d81480483ef08
Soria <soriasayer@gmail.com>
2020-03-22 11:39:08 +0100

    Buttons: Creating delete edit and create stream buttons.

--- src/component/streams/StreamList.js
+++ src/component/streams/StreamList.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {fetchStreams} from '../../actions'
+import { Link } from 'react-router-dom'
+import { fetchStreams } from '../../actions'
 
 class StreamList extends Component {
     componentDidMount() {
         this.props.fetchStreams()
-        console.log('it works here')
+    }
+
+    renderAdmin = ( stream ) => {
+        if ( stream.userId === this.props.currentUserId ) {
+            return (
+                <div className='right floated content'>
+                    <button className='ui button primary'>
+                        Edit
+                    </button>
+                    <button className='ui button negative'>
+                        Delete
+                    </button>
+                </div>
+            )
+        }
     }
 
     renderList = () => {
         return this.props.streams.map( stream => {
             return (
                 <div className='item' key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className='large middle aligned icon camera' />
                     <div className='content'>
                         {stream.title}
@@ -24,6 +40,17 @@ class StreamList extends Component {
         })
     }
 
+    renderCreate = () => {
+        if(this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to='/streams/new' className='ui button primary'>
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
     render() {
         return (
             <div>
@@ -31,15 +58,18 @@ class StreamList extends Component {
                 <div className='ui celled list'>
                     {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
         )
     }
 }
 
-const mapStateToProps = ( { streams } ) => {
+const mapStateToProps = ( { streams, auth } ) => {
     
     return {
-        streams: Object.values(streams)
+        streams: Object.values( streams ),
+        currentUserId: auth.userId,
+        isSignedIn: auth.isSignedIn
     }
 }
 
