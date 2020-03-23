51c538e5822e4839285f1257d636f4c3389f8dab
Soria <soriasayer@gmail.com>
2020-03-23 17:52:40 +0100

    Woriking on ShowStream component.

--- src/component/streams/StreamShow.js
+++ src/component/streams/StreamShow.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
+import streams from '../../actions/api/streams'
 
-const StreamShow = () => {
+class StreamShow extends Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.id)
+    }
 
-    return(
-        <div>StreamShow</div>
-    )
+    render() {
+      
+        if(!this.props.stream) {
+            return <div>Loading</div>
+        }
+
+        const {title, description} = this.props.stream
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
 }
 
-export default StreamShow
+const mapStateToProps = ( { streams }, ownProps ) => {
+    const {id} = ownProps.match.params
+    return {
+        stream: streams[id],
+        id
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchStream }
+)( StreamShow )
